e0ce3313fc7b4a66a99ea65aee2de4829b884d0d
Owen Cruse <ocruse7@gmail.com>
2019-11-15 04:56:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+def oxford_comma( array )
+  
+    if array.size == 1
+      return array.join
+    elsif array.size == 2
+      return array.join( " and " )
+    elsif array.length >= 3
+      array[-1] = "and #{array[-1]}"
+      return array.join(", ")
+    end
+    
 end
